4291b391b371c3057bdf49123c9976e089796051
Karen <karenkbutarello@gmail.com>
2025-07-04 09:43:53 -0300

    .

--- src/test/java/com/example/hotel/controller/HospedeControllerTest.java
+++ src/test/java/com/example/hotel/controller/HospedeControllerTest.java
@@ -27,7 +27,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class HospedeControllerTest {
+public class HospedeControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
